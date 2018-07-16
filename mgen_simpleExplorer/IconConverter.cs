3275d799ccc56970d80e6e1360b27a9ab4c1c67d
Harry-UW <41180087+Harry-UW@users.noreply.github.com>
2018-07-15 22:50:14 -0400

    Initial commit

---
+++ mgen_simpleExplorer/IconConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace Explorer
+
+{
+    class IconConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var vm = (FileSystemObjectViewModel)value;
+            return IconExtractor.GetIcon(vm.Path, true, vm.Type == FileSystemObjectType.Folder);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
